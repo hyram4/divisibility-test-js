2a6012883f184999643e5ceec1e57978b6a4baf2
hyram4 <hnjagimwagi@gmail.com>
2022-01-25 12:43:38 +0300

    full commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let i;
+for(i=1;i<=100; i++){
+     if(i%3==0 && i%5==0){
+        console.log("fiz buzz");
+        }
+   else if(i%3==0){
+        console.log("fiz");
+    } else if(i%5==0){
+        console.log("buzz");
+    } 
+     else{
+        console.log(i);
+     } 
+}
